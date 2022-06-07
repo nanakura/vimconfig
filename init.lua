57bbdc7c376b19770e611879b32a93bad5c88ee9
Monody-x <huanbinxiao52@gmail.com>
2022-06-07 17:23:25 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -405,8 +405,8 @@ vim.api.nvim_set_keymap("x", "<leader>u", "<Plug>kommentary_visual_decrease", {}
 require'FTerm'.setup({
     border = 'double',
     dimensions  = {
-        height = 0.9,
-        width = 0.9,
+        height = 0.7,
+        width = 0.8,
     },
 })
 vim.keymap.set('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>')
