a316ca194ec18db1c8d2cbf778e65f2131706882
xMonody <2304925521@qq.com>
2022-07-08 17:35:11 +0800

    cmp

--- init.lua
+++ init.lua
@@ -139,7 +139,7 @@ vim.diagnostic.config({
         virtual_text = true,
         signs = true,
         underline = true,
-        update_in_insert = false,
+        update_in_insert = true,
         severity_sort = false,
     }
 })
