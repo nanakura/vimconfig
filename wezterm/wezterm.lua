11fa90f3fb35f97b6c20950a720f5b666e5983e7
2304925521@qq.com <one@localhost.localdomain>
2022-09-22 17:22:45 +0800

    add nvim tree

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+
+return {
+	colors = custom,
+	--color_scheme = "Gogh (Gogh)",
+	color_scheme = "Grape",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	font = wezterm.font 'SauceCodePro Nerd Font Mono',
+	font_size=11,
+}
